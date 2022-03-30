f34ead24a5eaba6b8966c7de4dbc9bb7cea4a868
Murad <11_rma@mail.ru>
2022-03-30 13:38:59 +0300

    seminar3

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Console.WriteLine("Введите пятизначное число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a < 99999 & a >= 10000)
+// {
+//     int xx = a / 1000;
+//     int num1 = xx / 10;
+//     int num2 = xx % 10;
+//     int yy = a % 100;
+//     int num4 = yy / 10;
+//     int num5 = yy % 10;
+//     if ((num1 == num5) & (num2 == num4))
+//     {
+//         Console.WriteLine(" является полиндромом ");
+
+//     }
+// else
+// {
+//         Console.WriteLine(" не является полиндромом ");
+//         }
+//         }
+// else
+// {
+//         Console.WriteLine(" не 5-ти значное число ");
+// }
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.WriteLine("Введите Координату первых точек ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату вторых точек ");
+
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double S = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+// Console.WriteLine(S);
+
+
+// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// Console.WriteLine(" Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i <= number; i++)
+// {
+// Console.WriteLine($" Куб числа {i} = {Math.Pow(i, 3)} ");
+// }
+
+
+
+
